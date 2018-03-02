9effde82b8ae15684eb516e8f8a2b112d052219b
Aswani Kumar Veeramallu <aswanikumar.hyd@gmail.com>
2018-03-02 07:11:43 -0600

    Update HomeBudgetRestApplication.java

--- home-budget-services/src/main/java/com/aswani/personal/HomeBudgetRestApplication.java
+++ home-budget-services/src/main/java/com/aswani/personal/HomeBudgetRestApplication.java
@@ -1,11 +1,16 @@
 package com.aswani.personal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeBudgetRestApplication {
+	private static final Logger log = LoggerFactory.getLogger(HomeBudgetRestApplication.class);
 	public static void main(String[] args) {
+		log.info("Application starting ...");
 		SpringApplication.run(HomeBudgetRestApplication.class, args);
+		
 	}
 }
